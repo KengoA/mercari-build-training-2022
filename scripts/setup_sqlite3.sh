906be7322517fb90d4a7998c9527e518f80b5ea6
KengoA <kengo@hey.com>
2022-05-15 00:12:26 +0900

    change column name for image to image_filename

--- scripts/setup_sqlite3.sh
+++ scripts/setup_sqlite3.sh
@@ -1,5 +1,5 @@
 # brew install sqlite
 # go get github.com/mattn/go-sqlite3
 cd db
-sqlite3 mercari-build-training.db "CREATE TABLE items (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) NOT NULL, category VARCHAR(255), image text);"
+sqlite3 mercari-build-training.db "CREATE TABLE items (id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, name VARCHAR(255) NOT NULL, category VARCHAR(255), image_filename text);"
 sqlite3 mercari-build-training.db .schema > schema.sql
